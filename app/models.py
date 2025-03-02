ef7d033db5ef7ef30af77f202acf60f6678ab2b3
Spyro1322 <spirarm@gmail.com>
2025-03-02 14:53:34 +0200

    changes

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Model(Base):
 
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4), index=True)
     name = Column(String, index=True)
-    version = Column(String, index=True)
-    accuracy = Column(Float)
-    file_path = Column(String)
-    created_at = Column(DateTime(timezone=True, nullable=False), default=func.now())
+    version = Column(String, index=True, nullable=False)
+    accuracy = Column(Float, nullable=False)
+    file_path = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=func.now())
